f2832da67dd1b3d312b98482759050e084817d7b
Pranav Pradeep <pranavpradeep786@gmail.com>
2024-05-06 11:16:03 +0530

    final

---
+++ api/index.js
@@ -0,0 +1,42 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRouter from './routes/user.routes.js';
+import authRouter from './routes/authroute.js'
+import listingRouter from './routes/listingroute.js'
+import cookieParser from 'cookie-parser';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO)
+.then(()=>{
+    console.log('Connected to MongDB :)');
+}).catch((err)=>{
+    console.log(err);
+})
+
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.listen(3000,()=>{
+    console.log("Server running at port 3000 !!!");
+});
+
+app.use("/api/user",userRouter);
+app.use("/api/auth",authRouter);
+app.use("/api/listing",listingRouter);
+
+
+app.use((err,req,res,next)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
